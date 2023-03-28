b5111738b78aa301890723223749b1d9d83f9d60
matows <mateusz.owsiak@student.pk.edu.pl>
2023-03-28 12:49:18 +0200

    Updated login and register views, added registration logic

--- frontend/src/views/login/Login.js
+++ frontend/src/views/login/Login.js
@@ -5,21 +5,23 @@ import { Link }from "react-router-dom";
 function Login() {
     return (
         <>
-            <div className={styles.container}>
-                <div className={styles.loginPanel}>
-                    <img id={styles.logo} src={logo}/>
-                    <p className={styles.mainText}>Zaloguj się na swoje konto</p>
-                    <form className={styles.loginForm}>
-                        <input id={styles.emailInput} placeholder="email" type="email"/>
-                        <input id={styles.passwordInput} placeholder="haslo" type="password"/>
-                        <button id={styles.loginButton}  type="submit">Zaloguj się</button>
-                    </form>
-                </div>
-                <div className={styles.registerPanel}>
-                    <p className={styles.firstText}>Nie masz jeszcze konta?</p>
-                    <p className={styles.secondText}>Załóż już teraz i korzystaj z świetnych funkcjonalości aplikacji!</p>
-                    <Link className={styles.link} to="/register"><button id={styles.registerButton} type="button">Załóż konto</button></Link>
-                    <img id={styles.logo2} src={logo2}/>
+            <div className={styles.main}>
+                <div className={styles.container}>
+                    <div className={styles.loginPanel}>
+                        <img id={styles.logo} src={logo}/>
+                        <p className={styles.mainText}>Zaloguj się na swoje konto</p>
+                        <form className={styles.loginForm}>
+                            <input id={styles.emailInput} placeholder="email" type="email" name="email"/>
+                            <input id={styles.passwordInput} placeholder="haslo" type="password" name="password"/>
+                            <button id={styles.loginButton}  type="submit">Zaloguj się</button>
+                        </form>
+                    </div>
+                    <div className={styles.registerPanel}>
+                        <p className={styles.firstText}>Nie masz jeszcze konta?</p>
+                        <p className={styles.secondText}>Załóż już teraz i korzystaj z świetnych funkcjonalości aplikacji!</p>
+                        <Link className={styles.link} to="/register"><button id={styles.registerButton} type="button">Załóż konto</button></Link>
+                        <img id={styles.logo2} src={logo2}/>
+                    </div>
                 </div>
             </div>
         </>
--- frontend/src/views/register/Register.js
+++ frontend/src/views/register/Register.js
@@ -2,27 +2,69 @@ import  logo  from '../../public/img/logo.png';
 import  logo2 from '../../public/img/logo2.svg';
 import styles from '../../public/modules/register.module.css';
 import {Link} from "react-router-dom";
-function Register(){
-    return(
-        <div className={styles.container}>
-            <div className={styles.loginPanel}>
-                <img id={styles.logo} src={logo}/>
-                <p className={styles.mainText}>Założ konto już teraz</p>
-                <form className={styles.loginForm}>
-                    <input id={styles.emailInput} placeholder="email" type="email"/>
-                    <input id={styles.passwordInput} placeholder="haslo" type="password"/>
-                    <input id={styles.repeatPasswordInput} placeholder="powtorz haslo" type="password"/>
-                    <button id={styles.registerButton} type="submit">Załóż konto</button>
-                 </form>
-            </div>
-            <div className={styles.registerPanel}>
-                <p className={styles.firstText}>Masz już konto?</p>
-                <p className={styles.secondText}>Zaloguj się i korzystaj z aplikacji!</p>
-                <Link className={styles.link} to="/login"><button id={styles.loginButton} type="button">Zaloguj się</button></Link>
-                <img id={styles.logo2} src={logo2}/>
-            </div>
-    </div>
+import React from 'react';
+import axios from 'axios';
 
+function Register(){
+
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [repeatPassword, setRepeatPassword] = React.useState('');
+
+    const handleEmailChange = (event) =>{
+        setEmail(event.target.value);
+        console.log(email);
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        console.log(password);
+    }
+    const handleRepeatPasswordChange = (event) => {
+        setRepeatPassword(event.target.value);
+        console.log(repeatPassword);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data= {
+            email: email,
+            password: password
+        };
+
+        axios.post('http://127.0.0.1:8000/api/create',data, {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then(response => {
+                console.log(response.data.message);
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+    }
+
+    return(
+        <div className={styles.main}>
+            <div className={styles.container}>
+                <div className={styles.loginPanel}>
+                    <img id={styles.logo} src={logo}/>
+                    <p className={styles.mainText}>Założ konto już teraz</p>
+                    <form className={styles.loginForm} onSubmit={handleSubmit}>
+                        <input id={styles.emailInput} placeholder="email" type="email" name="email" onChange={handleEmailChange} required/>
+                        <input id={styles.passwordInput} placeholder="haslo" type="password" name="password" onChange={handlePasswordChange} required/>
+                        <input id={styles.repeatPasswordInput} placeholder="powtorz haslo" type="password" name="repeat_password" onChange={handleRepeatPasswordChange} required/>
+                        <button id={styles.registerButton} type="submit">Załóż konto</button>
+                     </form>
+                </div>
+                <div className={styles.registerPanel}>
+                    <p className={styles.firstText}>Masz już konto?</p>
+                    <p className={styles.secondText}>Zaloguj się i korzystaj z aplikacji!</p>
+                    <Link className={styles.link} to="/login"><button id={styles.loginButton} type="button">Zaloguj się</button></Link>
+                    <img id={styles.logo2} src={logo2}/>
+                </div>
+            </div>
+        </div>
     );
 }
 
