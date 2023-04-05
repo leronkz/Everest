f527fda2f79fb75f457189fbaebae97b251732d2
matows <mateusz.owsiak@student.pk.edu.pl>
2023-04-05 21:26:54 +0200

    [Added] Adding user and user's data functionality

---
+++ backend/src/Repository/UserDataRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use App\Entity\UserData;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<UserData>
+ *
+ * @method UserData|null find($id, $lockMode = null, $lockVersion = null)
+ * @method UserData|null findOneBy(array $criteria, array $orderBy = null)
+ * @method UserData[]    findAll()
+ * @method UserData[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class UserDataRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserData::class);
+    }
+
+    public function addUserData(UserData $userData): void{
+        $this->getEntityManager()->persist($userData);
+        $this->getEntityManager()->flush();
+    }
+//    public function remove(User $entity, bool $flush = false): void
+//    {
+//        $this->getEntityManager()->remove($entity);
+//
+//        if ($flush) {
+//            $this->getEntityManager()->flush();
+//        }
+//    }
+
+//    /**
+//     * @return User[] Returns an array of User objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('u.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?User
+//    {
+//        return $this->createQueryBuilder('u')
+//            ->andWhere('u.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
